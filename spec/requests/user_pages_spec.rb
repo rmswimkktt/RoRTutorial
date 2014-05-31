265f0a9ccbe82eaeec189b768ec5c4c04665c6ea
D.Maruta <rmswimkktt@gmail.com>
2014-05-31 13:53:05 +0900

    テストのtypoを修正

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,7 +5,7 @@ describe "UserPages" do
   subject { page }
 
   describe "profile page" do
-    let(:user) { FacrotyGirl.create(:user) }
+    let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
 
     it { should have_content(user.name) }
