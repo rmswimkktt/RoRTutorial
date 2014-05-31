ed025fb4cc9425dd893a0b6f025d2e4964c55099
D.Maruta <rmswimkktt@gmail.com>
2014-05-31 15:10:24 +0900

    Userコントローラにcreateを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #保存の成功をここで扱う
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
