e7625492f3a073b2c1d9f0630f990b52c6818d28
D.Maruta <rmswimkktt@gmail.com>
2014-05-31 15:29:36 +0900

    エラーメッセージを表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     if @user.save
       #保存の成功をここで扱う
     else
